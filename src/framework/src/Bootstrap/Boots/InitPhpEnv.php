38467ecb55f931d92b299b1d2b4cc63bd215eb88
inhere <in.798@qq.com>
2018-04-15 10:45:23 +0800

    fix: bug fixed for array heleper

--- src/framework/src/Bootstrap/Boots/InitPhpEnv.php
+++ src/framework/src/Bootstrap/Boots/InitPhpEnv.php
@@ -6,11 +6,7 @@ use Swoft\Bean\Annotation\Bootstrap;
 
 /**
  * @Bootstrap(order=1)
- * @uses      InitMbFunsEncoding
- * @version   2017-11-02
  * @author    huangzhhui <huangzhwork@gmail.com>
- * @copyright Copyright 2010-2017 Swoft software
- * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
  */
 class InitMbFunsEncoding implements Bootable
 {
@@ -19,6 +15,7 @@ class InitMbFunsEncoding implements Bootable
      */
     public function bootstrap()
     {
-        mb_internal_encoding("UTF-8");
+        \mb_internal_encoding('UTF-8');
+        \date_default_timezone_set('Asia/Shanghai');
     }
 }
