514c61fb18f05eb7617ccbe9a35d10a542ba994f
Inhere <inhere@users.noreply.github.com>
2018-04-22 00:58:48 +0800

    Update XmlHelper.php

--- src/framework/src/Helper/XmlHelper.php
+++ src/framework/src/Helper/XmlHelper.php
@@ -45,13 +45,16 @@ class XmlHelper
     {
         $res = [];
         //如果为空,一般是xml有空格之类的,导致解析失败
-        $data = @(array)simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
+        $data = @(array)\simplexml_load_string($xml, 'SimpleXMLElement', \LIBXML_NOCDATA | \LIBXML_NOBLANKS);
+        
         if (isset($data[0]) && $data[0] ===false) {
             $data = null;
         }
+        
         if ($data) {
             $res = self::parseToArray($data);
         }
+        
         return $res;
     }
 
@@ -62,12 +65,12 @@ class XmlHelper
     protected static function parseToArray($data)
     {
         $res = null;
-        if (is_object($data)) {
+        if (\is_object($data)) {
             $data = (array)$data;
         }
         if (is_array($data)) {
             foreach ($data as $key=>$val) {
-                if (is_iterable($val)) {
+                if (\is_iterable($val)) {
                     $res[$key] = self::parseToArray($val);
                 } else {
                     $res[$key] = $val;
@@ -84,19 +87,23 @@ class XmlHelper
     public static function arrayToXml(array $data)
     {
         $xml = '';
+        
         if (!empty($data)) {
             foreach ($data as $key=>$val) {
-                $xml.="<$key>";
-                if (is_iterable($val)) {
-                    $xml.=self::arrayToXml($val);
-                } elseif (is_numeric($val)) {
-                    $xml.=$val;
+                $xml .= "<$key>";
+                
+                if (\is_iterable($val)) {
+                    $xml .= self::arrayToXml($val);
+                } elseif (\is_numeric($val)) {
+                    $xml .= $val;
                 } else {
-                    $xml.=sprintf('<![CDATA[%s]]>', $val);
+                    $xml .= \sprintf('<![CDATA[%s]]>', $val);
                 }
-                $xml.="</$key>";
+                
+                $xml .= "</$key>";
             }
         }
+        
         return $xml;
     }
 }
