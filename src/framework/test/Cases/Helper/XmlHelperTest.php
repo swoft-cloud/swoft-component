f5060351cb455d956beb2c18280bb17c43e98c80
Huangzhhui <huangzhwork@gmail.com>
2018-04-22 00:58:14 +0800

    Update XmlHelperTest.php
    Format

--- src/framework/test/Cases/Helper/XmlHelperTest.php
+++ src/framework/test/Cases/Helper/XmlHelperTest.php
@@ -10,18 +10,20 @@ class XmlHelperTest extends AbstractTestCase
      * @test
      * @covers XmlHelper::encode()
      */
-    public function testEncode(){
-        $data = ['touser'=>['hello'=>'world'],'test'=>1];
+    public function testEncode()
+    {
+        $data = ['touser' => ['hello' => 'world'], 'test'=>1];
         $res = XmlHelper::encode($data);
-        $this->assertStringStartsWith("<xml><touser>",$res);
-        $this->assertStringEndsWith("</test></xml>",$res);
+        $this->assertStringStartsWith("<xml><touser>", $res);
+        $this->assertStringEndsWith("</test></xml>", $res);
     }
 
     /**
      * @test
      * @covers XmlHelper::decode()
      */
-    public function testDecode(){
+    public function testDecode()
+    {
         $xml = "<xml><touser><hello><![CDATA[world]]></hello></touser><test>1</test></xml>";
         $res = XmlHelper::decode($xml);
         $this->assertArraySubset(['touser' => ['hello' => 'world']], $res);
