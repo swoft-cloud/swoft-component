bb834b5ed8dd16a61ad5489c96b4742e1a461655
Huangzhhui <huangzhwork@gmail.com>
2018-04-24 23:03:57 +0800

    Update RouterMiddleware.php
    Format

--- src/rpc-server/src/Middleware/RouterMiddleware.php
+++ src/rpc-server/src/Middleware/RouterMiddleware.php
@@ -5,19 +5,13 @@ namespace Swoft\Rpc\Server\Middleware;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Swoft\App;
 use Swoft\Bean\Annotation\Bean;
 use Swoft\Http\Message\Middleware\MiddlewareInterface;
 
 /**
- * service router
+ * Service Router
  *
  * @Bean()
- * @uses      RouterMiddleware
- * @version   2017年11月26日
- * @author    stelin <phpcrazy@126.com>
- * @copyright Copyright 2010-2016 swoft software
- * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
  */
 class RouterMiddleware implements MiddlewareInterface
 {
@@ -27,7 +21,7 @@ class RouterMiddleware implements MiddlewareInterface
     const ATTRIBUTE = 'serviceHandler';
 
     /**
-     * get handler from router
+     * Get handler from router
      *
      * @param \Psr\Http\Message\ServerRequestInterface $request
      * @param \Psr\Http\Server\RequestHandlerInterface $handler
@@ -38,18 +32,18 @@ class RouterMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // service data
+        // Service data
         $data = $request->getAttribute(PackerMiddleware::ATTRIBUTE_DATA);
 
-        $method    = $data['method']?? '';
-        $version   = $data['version']?? '';
-        $interface = $data['interface']?? '';
+        $method    = $data['method'] ?? '';
+        $version   = $data['version'] ?? '';
+        $interface = $data['interface'] ?? '';
 
         /* @var \Swoft\Rpc\Server\Router\HandlerMapping $serviceRouter */
-        $serviceRouter  = App::getBean('serviceRouter');
+        $serviceRouter  = \bean('serviceRouter');
         $serviceHandler = $serviceRouter->getHandler($interface, $version, $method);
 
-        // deliver service data
+        // Deliver service data
         $request = $request->withAttribute(self::ATTRIBUTE, $serviceHandler);
 
         return $handler->handle($request);
