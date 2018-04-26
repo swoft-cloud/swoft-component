dada747ef5080a2973aa91de614197d4c90f8dd4
inhere <in.798@qq.com>
2018-04-26 09:41:29 +0800

    up: implement the psr/container interface

---
+++ src/framework/src/Exception/ContainerException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Swoft\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * Class ContainerException
+ * @package Swoft\Exception
+ */
+class ContainerException extends RuntimeException implements ContainerExceptionInterface
+{
+
+}
