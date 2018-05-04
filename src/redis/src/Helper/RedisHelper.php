28745029ee18fc65d31a5c677218377600e80c16
ott321 <ott321@yeah.net>
2018-05-04 14:23:30 +0800

    fix redis auth bug(tcp-uri)

---
+++ src/redis/src/Helper/RedisHelper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sl
+ * Date: 2018/5/4
+ * Time: 下午2:17
+ * @author April2 <ott321@yeah.net>
+ */
+
+namespace Swoft\Redis\Helper;
+
+
+class RedisHelper
+{
+
+}
---
+++ src/redis/test/Cases/RedisReadWriteTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sl
+ * Date: 2018/5/4
+ * Time: 下午1:30
+ * @author April2 <ott321@yeah.net>
+ */
+
+namespace SwoftTest\Redis;
+
+
+class RedisReadWriteTest
+{
+
+}
