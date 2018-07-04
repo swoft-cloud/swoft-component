1a67d73b33684646cba9c26679bd8f1a4541ccac
黄朝晖 <huangzhwork@gmail.com>
2018-07-04 12:44:01 +0800

    Update CustomValidatorParser.php
    format

--- src/framework/src/Bean/Parser/CustomValidatorParser.php
+++ src/framework/src/Bean/Parser/CustomValidatorParser.php
@@ -1,18 +1,21 @@
 <?php
-
+/**
+ * This file is part of Swoft.
+ *
+ * @link https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
 namespace Swoft\Bean\Parser;
 
 use Swoft\Bean\Annotation\CustomValidator;
 use Swoft\Bean\Collector\ValidatorCollector;
 
 /**
- * the parser of custom validator
+ * Class CustomValidatorParser
  *
- * @uses      CustomValidatorParser
- * @version   2018年07月02日
- * @author    limx <715557344@qq.com>
- * @copyright Copyright 2010-2016 swoft software
- * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ * @package Swoft\Bean\Parser
  */
 class CustomValidatorParser extends AbstractParser
 {
@@ -22,17 +25,15 @@ class CustomValidatorParser extends AbstractParser
      * @param string          $propertyName
      * @param string          $methodName
      * @param string|null     $propertyValue
-     *
      * @return null
      */
-    public function parser(
+    public function parser (
         string $className,
         $objectAnnotation = null,
         string $propertyName = '',
         string $methodName = '',
         $propertyValue = null
-    )
-    {
+    ) {
         ValidatorCollector::collect($className, $objectAnnotation, $propertyName, $methodName, $propertyValue);
         return null;
     }
