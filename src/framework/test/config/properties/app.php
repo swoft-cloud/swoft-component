bb8515d3fdd7ac15513a1cde243dac14127188d7
李铭昕 <715557344@qq.com>
2018-08-06 10:47:39 +0800

    增加用户自定义组件配置 (https://github.com/swoft-cloud/swoft-component/pull/150)
    * componentNamespaces可额外配置
    
    * 修改命名
    
    * Update app.php
    
    * Update AnnotationResource.php
    
    * 增加自定义组件 命名空间注册
    
    * 增加自定义组件主动设置组件地址
    
    * 修改注释

--- src/framework/test/config/properties/app.php
+++ src/framework/test/config/properties/app.php
@@ -11,12 +11,17 @@ return [
     'version' => '1.0',
     'autoInitBean' => true,
     'beanScan' => [
-        'SwoftTest\\Aop' => BASE_PATH . '/Cases/Aop',
         'SwoftTest\\Bean' => BASE_PATH . '/Cases/Bean',
         'SwoftTest\\Pool' => BASE_PATH . '/Cases/Pool',
     ],
     'bootScan' => [],
     'env' => 'Base',
+    'components' => [
+        'custom' => [
+            'SwoftTest' => BASE_PATH . '/Cases',
+            'SwoftTest'
+        ]
+    ],
     'provider' => require __DIR__ . DS . 'provider.php',
     'test' => require __DIR__ . DS . 'test.php',
 ];
