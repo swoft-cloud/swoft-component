7709a3458bd376db4f1a4a9878845d619b9263b1
李铭昕 <715557344@qq.com>
2018-09-12 11:57:57 +0800

    当实体主键为null时，再覆盖主键

--- src/db/src/Executor.php
+++ src/db/src/Executor.php
@@ -40,8 +40,12 @@ class Executor
         // Set Primary Id to Entity
         $query->addDecorator(function ($primaryId) use ($entity, $className) {
             list(, , $idColumn) = self::getTable($className);
+            // When Primary Id is auto increment
+            $getter = 'get' . StringHelper::camel($idColumn, false);
             $setter = 'set' . StringHelper::camel($idColumn, false);
-            method_exists($entity, $setter) && $entity->$setter($primaryId);
+            if (method_exists($entity, $getter) && method_exists($entity, $setter) && $entity->$getter() === null) {
+                $entity->$setter($primaryId);
+            }
             return $primaryId;
         });
         return $query->insert($fields);
