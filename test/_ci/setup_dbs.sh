4e1fc01edf2a782956691b7489e7f80dd9ac1cf9
李铭昕 <715557344@qq.com>
2018-12-07 14:52:28 +0800

    Fix BUG

--- test/_ci/setup_dbs.sh
+++ test/_ci/setup_dbs.sh
@@ -7,7 +7,7 @@ echo -e "Create MySQL database..."
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
 cat "${TRAVIS_BUILD_DIR}/test/_ci/mysql.sql" | mysql -u root test
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS test2 charset=utf8mb4 collate=utf8mb4_unicode_ci;"
-cat "${TRAVIS_BUILD_DIR}/test/_ci/mysql2.sql" | mysql -u root test
+cat "${TRAVIS_BUILD_DIR}/test/_ci/mysql2.sql" | mysql -u root test2
 
 echo -e "Done\n"
 
