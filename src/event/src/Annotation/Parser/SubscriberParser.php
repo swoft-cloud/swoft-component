5421d6ebded0c4aaf7844ec92bce0fa4ccbe8916
inhere <in.798@qq.com>
2019-02-01 22:31:52 +0800

    update: add more unit tests for event manager

---
+++ src/event/src/Annotation/Parser/SubscriberParser.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2019-02-01
+ * Time: 20:03
+ */
+
+namespace Swoft\Event\Annotation\Parser;
+
+use Doctrine\Common\Annotations\AnnotationException;
+use Swoft\Annotation\Annotation\Mapping\AnnotationParser;
+use Swoft\Annotation\Annotation\Parser\Parser;
+use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Event\Annotation\Mapping\Subscriber;
+
+/**
+ * Class ListenerParser
+ * @since 2.0
+ * @package Swoft\Event\Annotation\Parser
+ *
+ * @AnnotationParser(Subscriber::class)
+ */
+class SubscriberParser extends Parser
+{
+    /**
+     * @param int      $type
+     * @param Subscriber $annotation
+     *
+     * @return array
+     * @throws AnnotationException
+     */
+    public function parse(int $type, $annotation): array
+    {
+        if ($type !== self::TYPE_CLASS) {
+            throw new AnnotationException('`@Subscriber` must be defined by class!');
+        }
+
+        return [$this->className, $this->className, Bean::SINGLETON, ''];
+    }
+}
