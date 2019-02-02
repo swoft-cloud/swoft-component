ac369cca212ca8cc9a4092dc82706c9e72d06e89
inhere <in.798@qq.com>
2019-02-02 17:52:09 +0800

    update: update some logic. add some interface

--- src/LoaderInterface.php
+++ src/LoaderInterface.php
@@ -16,24 +16,4 @@ interface LoaderInterface
      * ]
      */
     public function getPrefixDirs(): array;
-
-    /**
-     * Disable or enable this component.
-     * @return bool
-     */
-    // public function enable(): bool;
-
-    /**
-     * @return array
-     * [
-     *  'name'        => 'my component',
-     *  'author'      => 'tom',
-     *  'version'     => '1.0.0',
-     *  'createAt'    => '2019.02.12',
-     *  'updateAt'    => '2019.04.12',
-     *  'description' => 'description for the component',
-     *  'homepage'    => 'https://github.com/inhere/some-component',
-     * ]
-     */
-    // public function getMetadata(): array;
 }
