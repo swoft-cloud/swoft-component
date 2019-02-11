fa7a6d8c3dae5c5637aeee28f28d92d5e1c16d39
inhere <in.798@qq.com>
2019-02-11 22:18:23 +0800

    update

--- src/stdlib/src/Helper/ComposerHelper.php
+++ src/stdlib/src/Helper/ComposerHelper.php
@@ -31,4 +31,45 @@ class ComposerHelper
 
         throw new \RuntimeException('Composer ClassLoader not found!');
     }
+
+    /**
+     * @param string $file
+     * @return array
+     */
+    public static function parseLockFile(string $file): array
+    {
+        if (!\is_file($file)) {
+            return [];
+        }
+
+        if (!$json = \file_get_contents($file)) {
+            return [];
+        }
+
+        /** @var array[] $data */
+        $data = \json_decode($json, true);
+        $components = [];
+
+        if (!$data || !isset($data['packages'])) {
+            return [];
+        }
+
+        foreach ($data['packages'] as $package) {
+            if (0 !== \strpos($package['name'], 'swoft/')) {
+                continue;
+            }
+
+            $components[] = [
+                'name' => $package['name'],
+                'version' => $package['version'],
+                'source' => $package['source'],
+                'require' => $package['require'] ?? [],
+                'description' => $package['description'],
+                'keywords' => $package['keywords'],
+                'time' => $package['time'],
+            ];
+        }
+
+        return $components;
+    }
 }
