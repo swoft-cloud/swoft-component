1cf17ab086104dde1897f9307abd8d6f7870f7ea
inhere <in.798@qq.com>
2019-02-15 10:00:14 +0800

    update some string helper methods

---
+++ src/stdlib/test/Helper/StringHelperTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SwoftTest\Stdlib\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Swoft\Stdlib\Helper\Str;
+
+/**
+ * Class StringHelperTest
+ * @since 2.0
+ */
+class StringHelperTest extends TestCase
+{
+    public function testGetClassName(): void
+    {
+        $name = Str::getClassName('App\Http\Controller\UserController', 'Controller');
+        $this->assertSame('user', $name);
+
+        $name = Str::getClassName('App\Http\Controller\UserController', '');
+        $this->assertSame('userController', $name);
+    }
+}
