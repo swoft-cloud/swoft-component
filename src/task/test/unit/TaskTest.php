e4c25946ed6938681d1da7938102954089be261b
stelin <794774870@qq.com>
2019-04-22 19:54:31 +0800

    Add task unit demo

---
+++ src/task/test/unit/TaskTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Task\Unit;
+
+/**
+ * Class TaskTest
+ *
+ * @since 2.0
+ */
+class TaskTest extends TestCase
+{
+    public function testCo()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ src/task/test/unit/TestCase.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Task\Unit;
+
+/**
+ * Class TestCase
+ *
+ * @since 2.0
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    
+}
