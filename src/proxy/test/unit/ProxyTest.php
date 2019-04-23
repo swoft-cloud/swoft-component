ce79067dcb054ec1d3eb4c446fbbaa2192eb2430
stelin <794774870@qq.com>
2019-04-23 14:58:48 +0800

    Add unit demo

---
+++ src/proxy/test/unit/ProxyTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Proxy\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ProxyTest
+ *
+ * @since 2.0
+ */
+class ProxyTest extends TestCase
+{
+    public function testA()
+    {
+        $this->assertTrue(true);
+    }
+}
--- src/tcp-server/test/bootstrap.php
+++ src/tcp-server/test/bootstrap.php
@@ -1,6 +1,20 @@
-<?php declare(strict_types=1);
-// Composer autoload
-$autoloadFile = dirname(__DIR__) . '/vendor/autoload.php';
-if (file_exists($autoloadFile)) {
-    require_once $autoloadFile;
+<?php
+// vendor at component dir
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require dirname(__DIR__) . '/vendor/autoload.php';
+    // application's vendor
+} elseif (file_exists(dirname(__DIR__, 5) . '/autoload.php')) {
+    /** @var \Composer\Autoload\ClassLoader $loader */
+    $loader = require dirname(__DIR__, 5) . '/autoload.php';
+
+    // need load testing psr4 config map
+    $componentDir  = dirname(__DIR__, 3);
+    $componentJson = $componentDir . '/composer.json';
+    $composerData  = json_decode(file_get_contents($componentJson), true);
+
+    foreach ($composerData['autoload-dev']['psr-4'] as $prefix => $dir) {
+        $loader->addPsr4($prefix, $componentDir . '/' . $dir);
+    }
+} else {
+    exit('Please run "composer install" to install the dependencies' . PHP_EOL);
 }
