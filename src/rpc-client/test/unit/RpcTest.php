35731e122dedde55c85a104141ec27bdafdfb5ac
stelin <794774870@qq.com>
2019-04-24 17:09:10 +0800

    Added rpc client and rpc server unit

---
+++ src/rpc-client/test/unit/RpcTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Rpc\Client\Unit;
+
+/**
+ * Class RpcTest
+ *
+ * @since 2.0
+ */
+class RpcTest extends TestCase
+{
+    public function testA()
+    {
+        $this->assertTrue(true);
+    }
+}
