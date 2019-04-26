88a7479ec1e3d645016e47e0879746f3d645d8a9
stelin <794774870@qq.com>
2019-04-26 15:18:28 +0800

    Add rpc context

---
+++ src/rpc-server/src/Listener/BeforeConnectListener.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+
+namespace Swoft\Rpc\Server\Listener;
+
+
+use Swoft\Context\Context;
+use Swoft\Event\Annotation\Mapping\Listener;
+use Swoft\Event\EventHandlerInterface;
+use Swoft\Event\EventInterface;
+use Swoft\Log\Helper\Log;
+use Swoft\Rpc\Server\ServiceConnectContext;
+use Swoft\Server\Swoole\SwooleEvent;
+use Swoft\Rpc\Server\ServiceServerEvent;
+
+/**
+ * Class BeforeConnectListener
+ *
+ * @since 2.0
+ *
+ * @Listener(event=ServiceServerEvent::BEFORE_CONNECT)
+ */
+class BeforeConnectListener implements EventHandlerInterface
+{
+    /**
+     * @param EventInterface $event
+     *
+     * @throws \ReflectionException
+     * @throws \Swoft\Bean\Exception\ContainerException
+     */
+    public function handle(EventInterface $event): void
+    {
+        list($server, $fd, $reactorId) = $event->getParams();
+        $context = ServiceConnectContext::new($server, $fd, $reactorId);
+
+        if (Log::getLogger()->isEnable()) {
+            $data = [
+                'event'       => SwooleEvent::CONNECT,
+                'uri'         => (string)$fd,
+                'requestTime' => microtime(true),
+            ];
+            $context->setMulti($data);
+        }
+
+        Context::set($context);
+    }
+}
