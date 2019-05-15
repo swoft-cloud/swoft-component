3686aa25cacdb444a3f77e3bc33f28005d3d8ec9
stelin <794774870@qq.com>
2019-05-15 13:56:59 +0800

    Modify push

--- subtree-push.sh
+++ subtree-push.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
-# git subtree split -P src/annotation -b annotation
-#
-#
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-# git remote add origin && git pull /Users/stelin/swoft/swoft/vendor/swoft/component/   && git push origin -f master
-#
-#
-#
-
-mkdir ${1} && cd ${1} && git init && git remote add origin git@github.com:swoft-cloud/swoft-${1}.git&& git pull /Users/stelin/swoft/swoft/vendor/swoft/component/  ${1} && git push origin -f master
-
-
-
-
 git subtree push --prefix=src/annotation git@github.com:swoft-cloud/swoft-annotation.git master --squash
+git subtree push --prefix=src/aop git@github.com:swoft-cloud/swoft-aop.git master --squash
+git subtree push --prefix=src/bean git@github.com:swoft-cloud/swoft-bean.git master --squash
+git subtree push --prefix=src/config git@github.com:swoft-cloud/swoft-config.git master --squash
+git subtree push --prefix=src/connection-pool git@github.com:swoft-cloud/swoft-connection-pool.git master --squash
+git subtree push --prefix=src/console git@github.com:swoft-cloud/swoft-console.git master --squash
+git subtree push --prefix=src/db git@github.com:swoft-cloud/swoft-db.git master --squash
+git subtree push --prefix=src/error git@github.com:swoft-cloud/swoft-error.git master --squash
+git subtree push --prefix=src/event git@github.com:swoft-cloud/swoft-event.git master --squash
+git subtree push --prefix=src/framework git@github.com:swoft-cloud/swoft-framework.git master --squash
+git subtree push --prefix=src/http-message git@github.com:swoft-cloud/swoft-http-message.git master --squash
+git subtree push --prefix=src/http-server git@github.com:swoft-cloud/swoft-http-server.git master --squash
+git subtree push --prefix=src/i18n git@github.com:swoft-cloud/swoft-i18n.git master --squash
+git subtree push --prefix=src/log git@github.com:swoft-cloud/swoft-log.git master --squash
+git subtree push --prefix=src/proxy git@github.com:swoft-cloud/swoft-proxy.git master --squash
+git subtree push --prefix=src/redis git@github.com:swoft-cloud/swoft-redis.git master --squash
+git subtree push --prefix=src/rpc git@github.com:swoft-cloud/swoft-rpc.git master --squash
+git subtree push --prefix=src/rpc-client git@github.com:swoft-cloud/swoft-rpc-client.git master --squash
+git subtree push --prefix=src/rpc-server git@github.com:swoft-cloud/swoft-rpc-server.git master --squash
+git subtree push --prefix=src/i18n git@github.com:swoft-cloud/swoft-i18n.git master --squash
+git subtree push --prefix=src/log git@github.com:swoft-cloud/swoft-log.git master --squash
+git subtree push --prefix=src/proxy git@github.com:swoft-cloud/swoft-proxy.git master --squash
+git subtree push --prefix=src/redis git@github.com:swoft-cloud/swoft-redis.git master --squash
+git subtree push --prefix=src/rpc git@github.com:swoft-cloud/swoft-rpc.git master --squash
+git subtree push --prefix=src/rpc-client git@github.com:swoft-cloud/swoft-rpc-client.git master --squash
+git subtree push --prefix=src/rpc-server git@github.com:swoft-cloud/swoft-rpc-server.git master --squash
+git subtree push --prefix=src/server git@github.com:swoft-cloud/swoft-server.git master --squash
+git subtree push --prefix=src/stdlib git@github.com:swoft-cloud/swoft-stdlib.git master --squash
+git subtree push --prefix=src/task git@github.com:swoft-cloud/swoft-task.git master --squash
+git subtree push --prefix=src/tcp-server git@github.com:swoft-cloud/swoft-tcp-server.git master --squash
+git subtree push --prefix=src/test git@github.com:swoft-cloud/swoft-test.git master --squash
+git subtree push --prefix=src/validator git@github.com:swoft-cloud/swoft-validator.git master --squash
+git subtree push --prefix=src/websocket-server git@github.com:swoft-cloud/swoft-websocket-server.git master --squash
