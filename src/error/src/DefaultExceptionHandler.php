cf089d701e3214a8194ac1edac3643615f5e4467
inhere <in.798@qq.com>
2019-05-18 10:15:05 +0800

    format error codes

--- src/error/src/DefaultExceptionHandler.php
+++ src/error/src/DefaultExceptionHandler.php
@@ -2,31 +2,27 @@
 
 namespace Swoft\Error;
 
-use function get_class;
-use function printf;
 use Swoft\Error\Contract\DefaultErrorHandlerInterface;
 use Throwable;
+use function get_class;
+use function printf;
 
 /**
  * Class DefaultExceptionHandler
+ *
  * @since 2.0
  */
 class DefaultExceptionHandler implements DefaultErrorHandlerInterface
 {
     /**
      * @param Throwable $e
+     *
      * @return void
      */
     public function handle(Throwable $e): void
     {
-        printf(
-            "(DEFAULT HANDLER)Exception(%s): %s\nAt File %s line %d\nTrace:\n%s\n",
-            get_class($e),
-            $e->getMessage(),
-            $e->getFile(),
-            $e->getLine(),
-            $e->getTraceAsString()
-        );
+        printf("(DEFAULT HANDLER)Exception(%s): %s\nAt File %s line %d\nTrace:\n%s\n", get_class($e), $e->getMessage(),
+            $e->getFile(), $e->getLine(), $e->getTraceAsString());
     }
 
     /**
