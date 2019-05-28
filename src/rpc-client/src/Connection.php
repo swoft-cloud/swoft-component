bef25b3cf12250c19c54f4184178cb2e9e5f03cd
stelin <794774870@qq.com>
2019-05-28 21:36:47 +0800

    修改状态

--- src/rpc-client/src/Connection.php
+++ src/rpc-client/src/Connection.php
@@ -76,7 +76,7 @@ class Connection extends AbstractConnection implements ConnectionInterface
             $connection->set($setting);
         }
 
-        if (!$connection->connect($host, $port)) {
+        if (!$connection->connect($host, (int)$port)) {
             throw new RpcClientException(
                 sprintf('Connect failed. host=%s port=%d', $host, $port)
             );
