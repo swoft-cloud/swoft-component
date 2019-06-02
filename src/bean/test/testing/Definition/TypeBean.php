e443cebdbd62f2312217f1c3083b000aededaf8a
stelin <794774870@qq.com>
2019-06-02 18:16:40 +0800

    Added bean unit

---
+++ src/bean/test/testing/Definition/TypeBean.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Bean\Testing\Definition;
+
+use Swoft\Bean\Annotation\Mapping\Bean;
+
+/**
+ * Class TypeBean
+ *
+ * @since 2.0
+ *
+ * @Bean("testTypeBean")
+ */
+class TypeBean
+{
+    /**
+     * @var
+     */
+    private $stringVar;
+
+    /**
+     * @var integer
+     */
+    private $intVar;
+
+    /**
+     * @var int
+     */
+    private $integerVar;
+
+    /**
+     * @var float
+     */
+    private $floatVar;
+
+    /**
+     * @var double
+     */
+    private $doubleVar;
+
+    /**
+     * @var array
+     */
+    private $arrayVar;
+
+    /**
+     * @return mixed
+     */
+    public function getStringVar()
+    {
+        return $this->stringVar;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIntVar()
+    {
+        return $this->intVar;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIntegerVar(): int
+    {
+        return $this->integerVar;
+    }
+
+    /**
+     * @return float
+     */
+    public function getFloatVar(): float
+    {
+        return $this->floatVar;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDoubleVar(): float
+    {
+        return $this->doubleVar;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArrayVar(): array
+    {
+        return $this->arrayVar;
+    }
+}
