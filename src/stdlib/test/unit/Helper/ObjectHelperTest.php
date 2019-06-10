bda3b1bf55fa26c79eff850139f710f6717ee002
inhere <in.798@qq.com>
2019-06-10 19:15:33 +0800

    fix var is not define

---
+++ src/stdlib/test/unit/Helper/ObjectHelperTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace SwoftTest\Stdlib\Unit\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Swoft\Stdlib\Helper\ObjectHelper;
+
+/**
+ * Class ObjectHelperTest
+ *
+ */
+class ObjectHelperTest extends TestCase
+{
+    public function test(): void
+    {
+        $samples = [
+            'int' => 0,
+            'string' => '',
+            'float' => 0,
+            'bool' => false,
+        ];
+
+        foreach ($samples as $sample => $want) {
+            $this->assertSame($want, ObjectHelper::getDefaultValue($sample));
+        }
+    }
+}
