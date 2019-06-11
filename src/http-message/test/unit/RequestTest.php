0d89c26ab244f149b593cd414e05978d3e18ce5e
inhere <in.798@qq.com>
2019-06-11 21:06:16 +0800

    add getter method for get accept language

--- src/http-message/test/unit/RequestTest.php
+++ src/http-message/test/unit/RequestTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
-    public function testA()
+    public function testA(): void
     {
         $this->assertTrue(true);
     }
