8aaa38c42dbf3d2be8f57dbda5c4431a6907b7c5
inhere <in.798@qq.com>
2019-06-20 22:32:22 +0800

    add more server events and modify some logic

---
+++ src/console/src/Exception/ConsoleErrorException.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Console\Exception;
+
+/**
+ * Class ConsoleErrorException
+ *
+ * @since 2.0
+ */
+class ConsoleErrorException extends CommandFlagException
+{
+
+}
