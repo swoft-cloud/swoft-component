f03bd83ddb90f70eeca029e13f30c6365a88177c
Carp Cai <563998753@qq.com>
2019-06-29 16:01:38 +0800

    Finish test unit of direcory helper.

---
+++ src/stdlib/test/unit/Helper/DirectoryHelperTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace SwoftTest\Stdlib\Unit\Helper;
+
+use function foo\func;
+use PHPUnit\Framework\TestCase;
+use Swoft\Stdlib\Helper\Dir;
+
+/**
+ * Class JsonHelperTest
+ *
+ * @since 2.0
+ */
+class DirectoryHelperTest extends TestCase
+{
+    /**
+     * Test unit of Create directory
+     * CarpCai <2019-06-27 13:20>
+     */
+    public function testMake(): void
+    {
+        $dir = 'test/directory_test/test_make_file';
+        Dir::make($dir, 777);
+        $this->assertSame(true, file_exists($dir) && is_dir($dir) && (@opendir($dir)));
+
+        $dir = 'test/directory_test/test_make_file_mode';
+        Dir::make($dir, 111);
+        $this->assertSame(true, file_exists($dir) && is_dir($dir) && (@opendir($dir) === false));
+    }
+}
