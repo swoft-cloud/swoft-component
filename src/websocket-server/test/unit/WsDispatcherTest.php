d1f66498f030ef0552c057bf1ed1c5631029b761
inhere <in.798@qq.com>
2019-07-06 15:03:19 +0800

    update: add some mock class for ws server

---
+++ src/websocket-server/test/unit/WsDispatcherTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace SwoftTest\WebSocket\Server\Unit;
+
+/**
+ * Class WsDispatcherTest
+ *
+ * @since 2.0
+ */
+class WsDispatcherTest extends WsServerTestCase
+{
+    public function testHandshake(): void
+    {
+
+    }
+}
