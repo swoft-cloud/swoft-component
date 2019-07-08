0f4903034a3f77f03aa352c5dee45f7f8bee2cd1
inhere <in.798@qq.com>
2019-07-08 23:42:33 +0800

    Squashed 'src/tcp/' content from commit 7d4fa7ad
    git-subtree-dir: src/tcp
    git-subtree-split: 7d4fa7adbbeca87488765bd91a11d60c0e8b0e8d

---
+++ src/Contract/PackerInterface.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Tcp\Contract;
+
+use Swoft\Tcp\Package;
+
+/**
+ * Interface PackerInterface - Data packer interface
+ *
+ * @since 2.0.3
+ */
+interface PackerInterface
+{
+    /**
+     * @return string
+     */
+    public static function getType(): string;
+
+    /**
+     * Encode Package object to string data.
+     *
+     * @param Package $package
+     *
+     * @return string
+     */
+    public function encode(Package $package): string;
+
+    /**
+     * Decode tcp package data to Package object
+     *
+     * @param string $data package data
+     *
+     * @return Package
+     */
+    public function decode(string $data): Package;
+}
---
+++ src/Exception/ProtocolException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Tcp\Exception;
+
+use RuntimeException;
+
+/**
+ * Class ProtocolException
+ *
+ * @since 2.0.3
+ */
+class ProtocolException extends RuntimeException
+{
+
+}
