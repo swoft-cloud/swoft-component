a43ffe4e9b4dcc050e036e7ac41f85f2675e5ab0
inhere <in.798@qq.com>
2019-08-05 18:20:13 +0800

    update some for stdlib func

--- src/stdlib/src/Helper/Functions.php
+++ src/stdlib/src/Helper/Functions.php
@@ -58,15 +58,10 @@ if (!function_exists('printr')) {
      */
     function printr(...$vars)
     {
-        $pos   = '';
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
-        $line  = $trace[0]['line'];
 
-        if (isset($trace[1]['class'])) {
-            $pos .= $trace[1]['class'];
-        } else {
-            $pos .= $trace[1]['file'];
-        }
+        $line = $trace[0]['line'];
+        $pos  = $trace[1]['class'] ?? $trace[0]['file'];
 
         if ($pos) {
             echo "CALL ON $pos($line):\n";
@@ -88,15 +83,10 @@ if (!function_exists('vdump')) {
      */
     function vdump(...$vars)
     {
-        $pos   = '';
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
-        $line  = $trace[0]['line'];
 
-        if (isset($trace[1]['class'])) {
-            $pos .= $trace[1]['class'];
-        } else {
-            $pos .= $trace[1]['file'];
-        }
+        $line = $trace[0]['line'];
+        $pos  = $trace[1]['class'] ?? $trace[0]['file'];
 
         if ($pos) {
             echo "CALL ON $pos($line):\n";
