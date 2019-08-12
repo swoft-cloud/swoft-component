ec221eab8ad90fdc980dd16dc3266902412ff3ce
inhere <in.798@qq.com>
2019-08-13 00:19:03 +0800

    update: add unit test for check ws handshake fail

--- src/websocket-server/test/unit/WsDispatcherTest.php
+++ src/websocket-server/test/unit/WsDispatcherTest.php
@@ -3,6 +3,7 @@
 namespace SwoftTest\WebSocket\Server\Unit;
 
 use function bean;
+use function get_class;
 use Swoft\WebSocket\Server\Exception\WsModuleRouteException;
 use Swoft\WebSocket\Server\WsDispatcher;
 use Throwable;
@@ -15,6 +16,8 @@ use Throwable;
 class WsDispatcherTest extends WsServerTestCase
 {
     /**
+     * php run.php -c src/websocket-server/phpunit.xml --filter testHandshake
+     *
      * @throws Throwable
      */
     public function testHandshake(): void
@@ -30,10 +33,11 @@ class WsDispatcherTest extends WsServerTestCase
         $this->assertTrue($status);
 
         try {
+            $conn->getRequest()->setUriPath('/not-exist-path');
             $dp->handshake($conn->getRequest(), $conn->getResponse());
         } catch (Throwable $e) {
-
+            $this->assertSame(WsModuleRouteException::class, get_class($e));
+            $this->assertSame('The requested websocket route path "/not-exist-path" is not exist!', $e->getMessage());
         }
-
     }
 }
