5a27704f1bd3a056b144e2ecbb4accf1c59680df
inhere <in.798@qq.com>
2019-08-12 23:28:46 +0800

    remove: rm log error on default error handler

--- src/error/src/DefaultExceptionHandler.php
+++ src/error/src/DefaultExceptionHandler.php
@@ -2,11 +2,8 @@
 
 namespace Swoft\Error;
 
-use ReflectionException;
-use Swoft\Bean\Exception\ContainerException;
 use Swoft\Error\Contract\DefaultErrorHandlerInterface;
 use Swoft\Log\Helper\CLog;
-use Swoft\Log\Helper\Log;
 use Throwable;
 use function get_class;
 
@@ -21,20 +18,13 @@ class DefaultExceptionHandler implements DefaultErrorHandlerInterface
      * @param Throwable $e
      *
      * @return void
-     * @throws ReflectionException
-     * @throws ContainerException
      */
     public function handle(Throwable $e): void
     {
-        Log::error($e->getMessage());
-        CLog::error(
-            "(DEFAULT HANDLER)Exception(%s): %s\nAt File %s line %d\nTrace:\n%s\n",
-            get_class($e),
-            $e->getMessage(),
-            $e->getFile(),
-            $e->getLine(),
-            $e->getTraceAsString()
-        );
+        // Log::error($e->getMessage());
+
+        CLog::error("(DEFAULT HANDLER)Exception(%s): %s\nAt File %s line %d\nTrace:\n%s\n", get_class($e),
+            $e->getMessage(), $e->getFile(), $e->getLine(), $e->getTraceAsString());
     }
 
     /**
