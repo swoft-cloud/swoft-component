b3b7145902cecb0e57cdc802a0e6d12edef3989c
inhere <in.798@qq.com>
2019-09-24 00:12:56 +0800

    add some common tool class to stdlib

---
+++ src/stdlib/src/PhpType.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Stdlib;
+
+/**
+ * Class PhpType
+ */
+final class PhpType
+{
+    // basic type
+    public const INT     = 'int';
+    public const BOOL    = 'bool';
+    public const BOOLEAN = 'boolean';
+    public const INTEGER = 'integer';
+    public const FLOAT   = 'float';
+    public const STRING  = 'string';
+
+    // complex type
+    public const ARRAY   = 'array';
+    public const OBJECT  = 'object';
+}
