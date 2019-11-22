3ac5c3f40dd24fb50761cf3fb7665750e42be11f
Airect <sunxiaojiao001@gmail.com>
2019-11-22 19:46:09 +0800

    Fix the result of  hmget can be false
    the result of hmget can be false while $keys is empty

--- src/redis/src/Connection/Connection.php
+++ src/redis/src/Connection/Connection.php
@@ -464,6 +464,7 @@ abstract class Connection extends AbstractConnection implements ConnectionInterf
     public function hMGet(string $key, array $keys): array
     {
         $values = $this->command('hMGet', [$key, $keys]);
+        if ($values === false) $values = [];
 
         $result = [];
         foreach ($values as $key => $value) {
