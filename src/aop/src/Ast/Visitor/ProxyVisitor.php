9bd2a5f33b61a7c3b4c0ba6c38cf3ba40e3ae4a2
inhere <in.798@qq.com>
2020-01-06 22:56:00 +0800

    update some logic for testing

--- src/aop/src/Ast/Visitor/ProxyVisitor.php
+++ src/aop/src/Ast/Visitor/ProxyVisitor.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types=1);
 
-
 namespace Swoft\Aop\Ast\Visitor;
 
 use PhpParser\Node;
--- test/testing/AutoLoader.php
+++ test/testing/AutoLoader.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 
-
 namespace SwoftTest\Component\Testing;
-
 
 use Swoft\SwoftComponent;
 
