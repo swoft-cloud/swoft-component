c9a3dd5c23e48be9e12f8beee9e3256d28c28d4a
李霖 <794774870@qq.com>
2020-01-10 14:59:00 +0800

    Update Connection.php

--- src/redis/src/Connection/Connection.php
+++ src/redis/src/Connection/Connection.php
@@ -464,7 +464,9 @@ abstract class Connection extends AbstractConnection implements ConnectionInterf
     public function hMGet(string $key, array $keys): array
     {
         $values = $this->command('hMGet', [$key, $keys]);
-        if ($values === false) $values = [];
+        if ($values === false) {
+            $values = [];
+        }
 
         $result = [];
         foreach ($values as $key => $value) {
