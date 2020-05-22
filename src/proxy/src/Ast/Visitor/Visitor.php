12e981cc167c918bfbc338add2e5ad18cc9a6791
inhere <in.798@qq.com>
2020-05-22 21:22:27 +0800

    up: format codes by run php-cs-fixer for src/proxy

--- src/proxy/src/Ast/Visitor/Visitor.php
+++ src/proxy/src/Ast/Visitor/Visitor.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
 
 namespace Swoft\Proxy\Ast\Visitor;
 
@@ -12,5 +20,4 @@ use Swoft\Proxy\Contract\VisitorInterface;
  */
 abstract class Visitor extends NodeVisitorAbstract implements VisitorInterface
 {
-
 }
